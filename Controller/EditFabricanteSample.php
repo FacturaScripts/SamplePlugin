c4fa44b71666fe70974e97bcdcdb2950c4b3fa60
Francesc Pineda Segarra <francesc.pineda.segarra@gmail.com>
2018-10-05 18:02:20 +0200

    First commit

---
+++ Controller/EditFabricanteSample.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * This file is part of FacturaScripts
+ * Copyright (C) 2017-2018 Carlos Garcia Gomez <carlos@facturascripts.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+namespace FacturaScripts\Plugins\SamplePlugin\Controller;
+
+use FacturaScripts\Core\Controller;
+
+/**
+ * Controller to edit a single item from the Fabricante model.
+ *
+ * This class is only a placeholder to don't break the generator and avoid copy&paste code.
+ *
+ * @author Carlos García Gómez <carlos@facturascripts.com>
+ * @author Artex Trading sa <jcuello@artextrading.com>
+ */
+class EditFabricanteSample extends Controller\EditFabricante
+{
+}
