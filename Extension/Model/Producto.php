573d24c4aed853e9584c5331c8fda280d7160b06
Abderrahim Darghal Belkacemi <abdedarghal111@gmail.com>
2025-03-11 11:11:35 +0100

    feat: added base methods and modified prefix and suffix

--- Extension/Model/Producto.php
+++ Extension/Model/Producto.php
@@ -22,21 +22,62 @@ use Closure;
 use FacturaScripts\Core\Tools;
 
 /**
- * Description of Producto
+ * Description of Producto.
+ * 
+ * Toda la documentación sobre modelos está en: https://facturascripts.com/publicaciones/extensiones-de-modelos
  *
  * @author Carlos Garcia Gomez <carlos@facturascripts.com>
  */
 class Producto
 {
 
+    protected string $prefix;
+    protected string $suffix;
+
     /**
      * Adds prefix() method to the model.
      */
     public function prefix() : Closure
     {
-        return function($prefix = 'PRE-') {
-            $this->referencia = $prefix . $this->referencia;
-            return $this->referencia;
+        return function() {
+            return $this->prefix . $this->referencia;
+        };
+    }
+
+    /**
+     * Adds suffix() method to the model.
+     */
+    public function suffix() : Closure
+    {
+        return function() {
+            return $this->referencia . $this->suffix;
+        }; 
+    }
+
+    /**
+     * The next methods are avalible in Model
+     */
+    public function clear() : Closure
+    {
+        return function(){
+            $this->prefix = 'PRE-';
+            $this->suffix = '-SUF';
+            Tools::log()->notice('This code is executed when a new "Producto" is created.');
+        };
+    }
+
+    public function delete() : Closure
+    {
+        return function(){
+            Tools::log()->notice('This code is executed after delete() method is called.');
+        };
+    }
+
+    public function deleteBefore() : Closure
+    {
+        return function(){
+            Tools::log()->notice('This code is executed before delete() method is called.'
+                . ' And return false to prevent delete().');
         };
     }
 
@@ -46,22 +87,64 @@ class Producto
             Tools::log()->notice('This code is executed after save() method is called.');
         };
     }
-
+    
     public function saveBefore() : Closure
     {
         return function() {
             Tools::log()->notice('This code is executed before save() method is called.'
-                . ' And return false can stop save().');
+                . ' And return false to prevent save().');
         };
     }
 
-    /**
-     * Adds suffix() method to the model.
-     */
-    public function suffix() : Closure
+    public function saveInsert() : Closure
     {
-        return function($suffix = '-SUF') {
-            return $this->referencia . $suffix;
+        return function() {
+            Tools::log()->notice('This code is executed after inserting "Producto" into the database.');
+        };
+    }
+
+    public function saveInsertBefore() : Closure
+    {
+        return function() {
+            Tools::log()->notice('This code is executed after inserting "Producto" into the database.'
+                . ' And return false to prevent saveInsert().');
+        };
+    }
+
+    public function saveUpdate() : Closure
+    {
+        return function() {
+            Tools::log()->notice('This code is executed after saveUpdate() method is called.');
+        };
+    }
+
+    public function saveUpdateBefore() : Closure
+    {
+        return function() {
+            Tools::log()->notice('This code is executed before saveUpdate() method is called.'
+                . ' And return false to prevent saveUpdate().');
+        };
+    }
+
+    public function test() : Closure
+    {
+        return function() {
+            Tools::log()->notice('This code is executed after test() method is called(data validation).');
+        };
+    }
+
+    public function testBefore() : Closure
+    {
+        return function() {
+            Tools::log()->notice('This code is executed before testBefore() method is called.'
+                . ' And return false to prevent saveUpdate().');
+        };
+    }
+
+    public function onChange() : Closure
+    {
+        return function() {
+            Tools::log()->notice('This code is executed before saveUpdate() method is called. It detects changes in columns');
         };
     }
 }
